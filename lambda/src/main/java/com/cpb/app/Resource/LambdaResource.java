90e7eff422288df4a0cfd663d166793ef0e0cee2
cuipengbo <cuipengbo@juran.com.cn>
2017-09-12 16:19:54 +0800

    update

--- lambda/src/main/java/com/cpb/app/Resource/LambdaResource.java
+++ lambda/src/main/java/com/cpb/app/Resource/LambdaResource.java
@@ -1,8 +1,10 @@
 package com.cpb.app.Resource;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.cpb.app.Dao.Person;
+import com.google.common.collect.Lists;
+
+import java.util.*;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -43,9 +45,45 @@ public class LambdaResource {
         List resultList = Arrays.asList(result);
         return resultList;
     }
+
+    /**
+     * sorted
+     * @param
+     */
+    public List lambda5() {
+        List<Person> persons = Lists.newArrayList();
+        for(int i=0;i<=5;i++) {
+            Person person = new Person(i,"name"+i);
+            person.setName("name"+i);
+            persons.add(person);
+        }
+        List<Person> result = persons.stream().limit(3).sorted(Comparator.comparing(Person::getName).reversed()).collect(Collectors.toList());
+        return result;
+    }
+
+    /**
+     * match
+     * @param
+     */
+    public Boolean lambda6() {
+        List<Person> person = Lists.newArrayList();
+        person.add(new Person("name"+1,10));
+        person.add(new Person("name"+2,19));
+        person.add(new Person("name"+3,20));
+        person.add(new Person("name"+4,30));
+        boolean isAllAdult = person.stream().allMatch(t -> t.getAge()>18);
+        System.out.println("all are adult?"+isAllAdult);
+        boolean isAnyChild = person.stream().anyMatch(t -> t.getAge()<12);
+        System.out.println("is has child?"+isAnyChild);
+        return isAllAdult;
+    }
+
     public static void main(String[] args) {
+        Random seed = new Random();
+        Supplier<Integer> random = seed::nextInt;
+        Stream.generate(random).limit(3).forEach(System.out::println);
+        Stream.iterate(0,t->t+3).limit(10).forEach(t->System.out.println(t));
         LambdaResource lambdaResource = new LambdaResource();
-        //() -> System.out.println("Hello Lambda Expressions")
-        System.out.println(lambdaResource.lambda4());
+        //System.out.println(lambdaResource.lambda6());
     }
 }
